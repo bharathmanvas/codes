19c215484cc5d1ecb494b88e559ec3d98dc7cf16
bharathmanvas <48153314+bharathmanvas@users.noreply.github.com>
2019-03-03 08:54:11 +0530

    vowel.py

---
+++ vowel.py
@@ -0,0 +1,9 @@
+x=("a","e","i","o","u")
+y=input("enter the alpabet.......")
+for vowels in x:
+    if(vowels==y):
+        print("the character is vowel")
+        break;
+    else:
+        print("it is not vowel")
+        break;
