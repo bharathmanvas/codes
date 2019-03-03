99b45d0cccb3de82481b9834c08205debfe0d481
bharathmanvas <48153314+bharathmanvas@users.noreply.github.com>
2019-03-03 09:05:01 +0530

    alphaornot.py

---
+++ alphaornot.py
@@ -0,0 +1,9 @@
+x=("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
+y=input("enter the character....")
+for vowels in x:
+    if(vowels==y):
+        print("alphabet")
+        break;
+    else:
+        print("it is not alphabet")
+        break;
